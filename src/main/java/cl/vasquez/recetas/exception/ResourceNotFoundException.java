1e94bae63e51fdd1ad8daad5898fc41694547312
DVasquez <166222526+davo-94@users.noreply.github.com>
2025-07-13 01:36:36 -0400

    Delete src directory

--- src/main/java/cl/vasquez/recetas/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package cl.vasquez.recetas.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException (String mensaje) {
-        super(mensaje); //Llama al constructor de RuntimeExcepcion con el mensaje de error
-    }
-
-}
